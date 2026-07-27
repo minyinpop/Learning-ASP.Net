8e3902b888b44a128ab03d52e4bf0ba1f1ade148
minyinpop <minyinpop@gmail.com>
2026-07-27 18:00:08 +0800

    初始化資料庫

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Learning_ASP.Net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
