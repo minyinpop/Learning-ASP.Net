98c491ce66e2b01486614dc9bf309b235d1a5a90
minyinpop <minyinpop@gmail.com>
2026-07-28 23:24:16 +0800

    串聯 Index 的前後端

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,9 +4,18 @@ namespace Learning_ASP.Net.Controllers;
 
 public class LoginController : Controller
 {
-    // GET
+    [HttpGet]
     public IActionResult Index()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(string username, string password)
+    {
+        Console.WriteLine(username);
+        Console.WriteLine(password);
+        
         return View();
     }
 }
