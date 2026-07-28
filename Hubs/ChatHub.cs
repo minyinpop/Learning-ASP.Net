67d7df91dcde20b217c68f3db8976246ac2fc0e0
minyinpop <minyinpop@gmail.com>
2026-07-29 02:17:57 +0800

    更改檔案

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,4 +4,10 @@ namespace Learning_ASP.Net.Hubs;
 
 public class ChatHub : Hub
 {
+    public Task Echo(string message)
+    {
+        Console.WriteLine($"收到：{message}");
+
+        return Task.CompletedTask;
+    }
 }
