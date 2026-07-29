001adf914537cba5f2dff797b36b47bbc23a3ebe
minyinpop <minyinpop@gmail.com>
2026-07-29 15:51:30 +0800

    更新程式

--- wwwroot/js/signalr.js
+++ wwwroot/js/signalr.js
@@ -1,1 +1,13 @@
-console.log(signalR);
+const connection = new signalR.HubConnectionBuilder()
+.withUrl("/chat")
+.build();
+
+async function connect()
+{
+    await connection.start();
+    console.log("Connected!");
+    
+    await connection.invoke("Echo", "Hello SignalR!");
+}
+
+connect();
