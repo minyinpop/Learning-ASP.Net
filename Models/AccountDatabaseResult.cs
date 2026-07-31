14ad9e27e4bdd0271406c01056e861633f831e8a
minyinpop <minyinpop@gmail.com>
2026-08-01 02:09:00 +0800

    新增結果

--- Models/AccountDatabaseResult.cs
+++ Models/AccountDatabaseResult.cs
@@ -2,6 +2,10 @@ namespace Learning_ASP.Net.Models;
 
 public enum AccountDatabaseResult
 {
+    // When user login account. ( 100 )
+    Login_Successfully = 101,
+    Login_AccountNotExists = 102,
+    
     // When user register account. ( 200 )
     AccountRegisterSuccessfully = 201,
     AccountIsAlreadyExists = 202,
