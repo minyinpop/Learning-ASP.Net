c96fc6ef21b113ae673b4f4b294355f2eb4b88bc
minyinpop <minyinpop@gmail.com>
2026-08-01 00:55:42 +0800

    新增帳號創建結果

---
+++ Models/AccountDatabaseResult.cs
@@ -0,0 +1,8 @@
+namespace Learning_ASP.Net.Models;
+
+public enum AccountDatabaseResult
+{
+    // When user register account. ( 200 )
+    AccountRegisterSuccessfully = 201,
+    AccountIsAlreadyExists = 202,
+}
