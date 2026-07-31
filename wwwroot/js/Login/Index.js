6ba7973ce1bc2c0ff37f29b5b8e3d450473af9c3
minyinpop <minyinpop@gmail.com>
2026-08-01 02:10:47 +0800

    實作登入的程式

--- wwwroot/js/Login/Index.js
+++ wwwroot/js/Login/Index.js
@@ -1,3 +1,6 @@
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
 const confirmButton = document.getElementById("confirmButton");
 const registerPageButton = document.getElementById("registerPageButton");
 
@@ -6,17 +9,78 @@ registerPageButton.addEventListener("click", OnClickRegisterPageButton);
 
 async function OnClickConfirmButton()
 {
-    const response = await fetch("/Login/Test",
-        {
-            method: "POST"
-        });
+    if (!await CheckEmail())
+    {
+        return;
+    }
     
-    await response.json();
+    if (!CheckPassword())
+    {
+        return;
+    }
     
-    console.log(response.message);
+    const fetchSettings = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: emailInput.value,
+            password: passwordInput.value
+        })
+    }
+    
+    const response = await fetch("/Login/TryLogin", fetchSettings);
+    
+    const json = await response.json();
+    
+    alert(json.message);
 }
 
 async function OnClickRegisterPageButton()
 {
     window.location.href = "/Register";
+}
+
+async function CheckEmail()
+{
+    if (CheckInputEmpty(emailInput, "電子信箱不得為空！"))
+    {
+        return false;
+    }
+
+    const response = await fetch("/Register/GetRegisterRules");
+
+    const rules = await response.json();
+
+    const emailRegex = new RegExp(rules.email);
+
+    if (!emailRegex.test(emailInput.value))
+    {
+        alert("請輸入正確的電子信箱格式！");
+        return false;
+    }
+
+    return true;
+}
+
+function CheckPassword()
+{
+    if (CheckInputEmpty(passwordInput, "密碼不得為空！"))
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+function CheckInputEmpty(inputElement, alertMessage)
+{
+    if (inputElement.value.trim() === "")
+    {
+        alert(alertMessage);
+        return true;
+    }
+    
+    return false;
 }
